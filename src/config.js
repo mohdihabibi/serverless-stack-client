0ca198687e48903c987ae2d69228e648ee929f57
Mohdi Habibi <mohdihabibi@gmail.com>
2020-04-25 01:26:34 -0700

    modify app with recent changes

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-west-1",
+    BUCKET: "notes-app-uploads-sjsu",
+  },
+  apiGateway: {
+    REGION: "us-west-1",
+    URL: "https://6ywgb7ag04.execute-api.us-west-1.amazonaws.com/dev",
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_fKpZONqOZ",
+    APP_CLIENT_ID: "56pgcskm4hqbbior45i1qbp1ka",
+    IDENTITY_POOL_ID: "us-west-2:bd7e248d-dcdd-41f6-a2f0-98201484ba4b",
+  },
+};
